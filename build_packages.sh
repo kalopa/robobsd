79d47a3b7c75f1350d756e5e7eb9258d2cb79868
Dermot Tynan <dtynan@kalopa.com>
2018-06-20 17:35:46 +0100

    Many changes to work with FreeBSD 11

--- build_packages.sh
+++ build_packages.sh
@@ -3,32 +3,33 @@
 set -e
 
 PACKAGES="\
-	ports-mgmt/pkg \
-	print/indexinfo \
-	converters/libiconv \
-	devel/libedit \
-	textproc/libyaml \
-	devel/gettext-runtime \
-	devel/gettext-tools \
-	devel/gmake \
-	net/rsync \
-	security/sudo \
-	lang/perl5.24 \
-	devel/p5-Locale-gettext \
-	misc/help2man \
-	print/texinfo \
-	devel/m4 \
-	devel/autoconf-wrapper \
-	devel/autoconf \
-	devel/automake-wrapper \
-	devel/automake \
-	lang/tcl86 \
-	lang/expect \
-	misc/dejagnu \
-	devel/libffi \
-	lang/ruby23 \
-	devel/ruby-gems \
-	databases/redis \
+	autoconf-2.69_1 \
+	autoconf-wrapper-20131203 \
+	automake-1.15.1 \
+	automake-wrapper-20131203 \
+	dejagnu-1.6.1 \
+	expect-5.45.4,1 \
+	gettext-runtime-0.19.8.1_1 \
+	gettext-tools-0.19.8.1 \
+	gmake-4.2.1_2 \
+	help2man-1.47.6 \
+	indexinfo-0.3.1 \
+	libedit-3.1.20170329_2,1 \
+	libffi-3.2.1_2 \
+	libiconv-1.14_11 \
+	libyaml-0.1.6_2 \
+	m4-1.4.18,1 \
+	p5-Locale-gettext-1.07 \
+	perl5.24-5.24.4 \
+	pkg-1.10.5_1 \
+	redis-4.0.9_1 \
+	rsync-3.1.3 \
+	ruby23-2.3.7,1 \
+	ruby24-gems-2.7.6 \
+	sudo-1.8.23_2 \
+	tcl86-8.6.8 \
+	texinfo-6.5,1 \
+	rubygem-bundler-1.16.1 \
 "
 
 rm -rf log/ packages/
@@ -37,8 +38,6 @@ mkdir -p log packages
 for package in $PACKAGES
 do
     echo ">>>> $package"
-    mkdir -p log/$package
-    make -C /usr/ports/$package -DBATCH package > log/$package/build.log 2>&1
-    cp /usr/ports/$package/work/pkg/${basic}*.txz packages/
+    pkg fetch -y -o packages $package
 done
 exit 0
