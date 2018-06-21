83dba9569e9da8c100590a497896460f33c9029e
Dermot Tynan <dtynan@kalopa.com>
2018-06-21 17:20:13 +0100

    More cleanup of the RoboBSD build system

--- get_packages.sh
+++ get_packages.sh
@@ -7,29 +7,36 @@ PACKAGES="\
 	autoconf-wrapper-20131203 \
 	automake-1.15.1 \
 	automake-wrapper-20131203 \
+	binutils-2.30_2,1 \
 	dejagnu-1.6.1 \
 	expect-5.45.4,1 \
+	gcc-ecj-4.5 \
+	gcc6-6.4.0_4 \
 	gettext-runtime-0.19.8.1_1 \
 	gettext-tools-0.19.8.1 \
 	gmake-4.2.1_2 \
+	gmp-6.1.2 \
 	help2man-1.47.6 \
 	indexinfo-0.3.1 \
 	libedit-3.1.20170329_2,1 \
 	libffi-3.2.1_2 \
 	libiconv-1.14_11 \
+	libunwind-20170615 \
 	libyaml-0.1.6_2 \
 	m4-1.4.18,1 \
+	mpc-1.1.0 \
+	mpfr-3.1.6 \
 	p5-Locale-gettext-1.07 \
-	perl5.24-5.24.4 \
-	pkg-1.10.5_1 \
-	redis-4.0.9_1 \
+	perl5-5.26.2 \
+	pkg-1.10.5 \
+	redis-3.2.11 \
 	rsync-3.1.3 \
-	ruby23-2.3.7,1 \
+	ruby-2.4.4,1 \
 	ruby24-gems-2.7.6 \
-	sudo-1.8.23_2 \
+	rubygem-bundler-1.16.1 \
+	sudo-1.8.22 \
 	tcl86-8.6.8 \
 	texinfo-6.5,1 \
-	rubygem-bundler-1.16.1 \
 "
 
 rm -rf log/ packages/
